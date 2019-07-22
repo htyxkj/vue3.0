3fb385172bcd2329a1ba1d4cc5144480a7c6270c
fengzejiang <fengzejiang1987@163.com>
2019-07-22 10:45:10 +0800

    没有初始化数据

--- src/classes/pub/CRecord.ts
+++ src/classes/pub/CRecord.ts
@@ -4,4 +4,8 @@ export default class CRecord {
     c_state:number = 0
     data:any
     subs:Array<CData> = []
+    constructor(state:number=0){
+        this.data = {}
+        this.c_state = state
+    }
 } 
