60fd17c0c5aed90fe97be4dad8b374dea3572498
LiZhengguo <lzg163email@163.com>
2019-12-13 17:43:13 +0800

    update

--- src/components/map/MyBaiMap.vue
+++ src/components/map/MyBaiMap.vue
@@ -1,35 +1,38 @@
-// <template>
-//     <div id="bMapDiv" style="width:100%;height:100%"></div>
-// </template>
-// <script lang="ts">
-// import { Component, Vue, Provide, Watch } from "vue-property-decorator";
-// import { State, Action, Getter, Mutation } from "vuex-class";
-// @Component({
-//   components: {
+<template>
+    <div id="bMapDiv" style="width:100%;height:100%"></div>
+</template>
+<script lang="ts">
+import baiMapJS from './js/b.js'
+import { Component, Vue, Provide, Watch } from "vue-property-decorator";
+import { State, Action, Getter, Mutation } from "vuex-class";
+@Component({
+  components: {
 
-//   }
-// }) 
-// export default class MyTianMap extends Vue {
-//     @Provide() bMap:any=null;
-//     @Provide() bZoom:number=12;
-//     created(){
+  }
+}) 
+export default class MyTianMap extends Vue {
+    @Provide() bMap:any=null;
+    @Provide() bZoom:number=12;
+    created(){
 
-//     }
-//     mounted(){
-//         this.createdMap();
-//     }
-//     createdMap(){
-//         console.log("初始化百度地图！")
-//         this.bMap = new BMap.Map('bMapDiv');
-//         this.bMap.centerAndZoom(new BMap.Point(116.40769, 39.89945), this.bZoom);
-//     }
-//     mapChnage(){
+    }
+    mounted(){
+        this.createdMap();
+    }
+    createdMap(){
+        baiMapJS.init().then((BMap:any) => {  
+            console.log("初始化百度地图！")
+            this.bMap = new BMap.Map('bMapDiv');
+            this.bMap.centerAndZoom(new BMap.Point(116.40769, 39.89945), this.bZoom);
+        })
+    }
+    mapChnage(){
 
-//     }  
-// }
-// </script>
-// <style scoped lang="scss" >
-// .myTab{
-//     height: 600px;
-// }
-// </style>
+    }  
+}
+</script>
+<style scoped lang="scss" >
+.myTab{
+    height: 600px;
+}
+</style>
--- src/components/map/MyTianMap.vue
+++ src/components/map/MyTianMap.vue
@@ -1,35 +1,40 @@
-// <template>
-//     <div id="tMapDiv" style="width:100%;height:100%"></div>
-// </template>
-// <script lang="ts">
-// import { Component, Vue, Provide, Watch } from "vue-property-decorator";
-// import { State, Action, Getter, Mutation } from "vuex-class";
-// @Component({
-//   components: {
+<template>
+    <div id="tMapDiv" style="width:100%;height:100%"></div>
+</template>
+<script lang="ts">
 
-//   }
-// }) 
-// export default class MyTianMap extends Vue {
-//     @Provide() tMap:any=null;
-//     @Provide() tZoom:number=12;
-//     created(){
+import tianMapJS from './js/t.js'
+import { Component, Vue, Provide, Watch } from "vue-property-decorator";
+import { State, Action, Getter, Mutation } from "vuex-class";
+@Component({
+  components: {
 
-//     }
-//     mounted(){
-//         this.createdMap();
-//     }
-//     createdMap(){
-//         console.log("初始化天地图！")
-//         this.tMap = new T.Map('tMapDiv');
-//         this.tMap.centerAndZoom(new T.LngLat(116.40769, 39.89945), this.tZoom);
-//     }
-//     mapChnage(){
+  }
+}) 
+export default class MyTianMap extends Vue {
+    @Provide() tMap:any=null;
+    @Provide() tZoom:number=12;
+    created(){
 
-//     }  
-// }
-// </script>
-// <style scoped lang="scss" >
-// .myTab{
-//     height: 600px;
-// }
-// </style>
+    }
+    mounted(){
+        this.createdMap();
+    }
+    createdMap(){
+        tianMapJS.init().then((T:any) => {  
+            console.log("初始化天地图！")
+            this.tMap = new T.Map('tMapDiv');
+            this.tMap.centerAndZoom(new T.LngLat(116.40769, 39.89945), this.tZoom);
+        });
+    }
+    mapChnage(){
+
+    }  
+    init(){}
+}
+</script>
+<style scoped lang="scss" >
+.myTab{
+    height: 600px;
+}
+</style>
