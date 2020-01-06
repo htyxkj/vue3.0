1a730afdddfb5533e159edc511251ee40f4c60ee
LiZhengguo <lzg163email@163.com>
2020-01-06 09:37:49 +0800

    审批流程修改

--- src/classes/cenv/CeaPars.ts
+++ src/classes/cenv/CeaPars.ts
@@ -3,11 +3,14 @@ export default class CeaPars{
     sbuid:string = ''
     yjcontext:any = ''
     statefr:any = 0
+    stateagr:any = 0
     stateto:any = 0
     bup:any = 1
     content:any = ''
     tousr:any = ''
     ckd:any = false
+    schk_mk:string = ''
+    sorg:string = ''
     constructor(param:any){
         const options={
             sid:'',
@@ -18,7 +21,9 @@ export default class CeaPars{
             bup:1,
             content:'',
             tousr:'',
-            ckd:false
+            ckd:false,
+            schk_mk:'',
+            sorg:''
           }
         Object.assign(this,options,param);
     }
