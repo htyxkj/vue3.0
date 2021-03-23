6d3cbfed017c4049a109e7994744bb99b8b019cc
LiZhengguo <lzg163email@163.com>
2021-03-23 18:11:58 +0800

    添加注释

--- src/classes/search/QueryEntity.ts
+++ src/classes/search/QueryEntity.ts
@@ -1,17 +1,24 @@
 import PageInfo from './PageInfo';
 
 export default class QueryEntity{
-    pcell:string
-    tcell:string
-    cont:string
-    mcont:string
-    orderBy:string
-    type:number = 0
-    page:PageInfo = new PageInfo()
-    values:Array<any> = []
+    pcell:string    //查询对象
+    tcell:string    //条件对象
+    cont:string     //条件
+    mcont:string    //初始条件
+    orderBy:string      //排序
+    type:number = 0     
+    page:PageInfo = new PageInfo()//分页信息
+    values:Array<any> = [] //数据
     oprid:number = 13;//13查询数据，14，查询一条记录（包含主子）
     groupV:string = ''
     polnk:string = ''
+    /**
+     * 
+     * @param _pcell 查询对象
+     * @param _tcell 条件对象
+     * @param _cont  条件
+     * @param _orderBy 排序
+     */
     constructor(_pcell:string,_tcell:string,_cont?:string,_orderBy?:string){
         this.pcell = _pcell
         this.tcell = _tcell
