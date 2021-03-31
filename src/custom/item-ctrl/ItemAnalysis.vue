a30d43174e4a5fce665ed51153f5d091338c2580
LiZhengguo <lzg163email@163.com>
2021-03-31 16:34:07 +0800

    bug修改

--- src/custom/item-ctrl/ItemAnalysis.vue
+++ src/custom/item-ctrl/ItemAnalysis.vue
@@ -1031,7 +1031,7 @@ export default class ItemAnalysis extends Vue {
     this.rightT1C1Con = JSON.parse(JSON.stringify(leftT1Con));
     this.rightT1C2Con = JSON.parse(JSON.stringify(leftT1Con));
 
-    let centerTC1Con = {
+    let centerTC1Con ={
       // 图表主标题
       title: {
         text: '', // 主标题文本，支持使用 \n 换行
@@ -1108,36 +1108,14 @@ export default class ItemAnalysis extends Vue {
           color: '#407bf3', // 水球未到的背景颜色
           opacity: 0.5
         },
-      },]
-      // series: [
-      //   {
-      //     type: "liquidFill",
-      //     name: "", // 系列名称，用于tooltip的显示，legend 的图例筛选
-      //     radius: "60%", // 水球图的半径
-      //     center: ["50%", "51%"], // 水球图的中心（圆心）坐标，数组的第一项是横坐标，第二项是纵坐标
-      //     // 水填充图的形状 circle 默认圆形  rect 圆角矩形  triangle 三角形
-      //     // diamond 菱形  pin 水滴状 arrow 箭头状  还可以是svg的path
-      //     shape: "circle",
-      //     phase: 0, // 波的相位弧度 不设置  默认自动
-      //     direction: "right", // 波浪移动的速度  两个参数  left 从右往左 right 从左往右
-      //     outline: {
-      //       show: true,
-      //       borderDistance: 0, // 边框线与图表的距离 数字
-      //       itemStyle: {
-      //         opacity: 1, // 边框的透明度   默认为 1
-      //         borderWidth: 1, // 边框的宽度
-      //         shadowBlur: 1, // 边框的阴影范围 一旦设置了内外都有阴影
-      //         shadowColor: "#fff", // 边框的阴影颜色,
-      //         borderColor: "#41dcd8", // 边框颜色
-      //       },
-      //     },
-      //     // 图形样式
-      //     itemStyle: {
-      //       opacity: 0.8 // 鼠标经过波浪颜色的透明度
-      //     }
-      //   },
-      //   data: [] // 系列中的数据内容数组
-      // }]
+        // 图形的高亮样式
+        emphasis: {
+          itemStyle: {
+            opacity: 0.8 // 鼠标经过波浪颜色的透明度
+          }
+        },
+        data: [] // 系列中的数据内容数组
+      }]
     }
     this.centerTC1Con =  JSON.parse(JSON.stringify(centerTC1Con))
     this.centerTC2Con =  JSON.parse(JSON.stringify(centerTC1Con))
