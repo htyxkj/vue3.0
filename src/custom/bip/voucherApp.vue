ee561f56b718b5f2ad7613ac53b463e3ad594df4
fengzejiang <fengzejiang1987@163.com>
2021-06-16 18:25:38 +0800

    修改凭证页面
    修改凭证页面

--- src/custom/bip/voucherApp.vue
+++ src/custom/bip/voucherApp.vue
@@ -11,27 +11,64 @@
                     </el-row>
                 </el-card>
                 <el-card>
+                    <vxe-toolbar>
+                        <template #buttons>
+                            <el-button style="margin-left:18px;" size="small" type="info" icon="el-icon-edit" @click="insertPZRow()">新增凭证行</el-button>
+                        </template>
+                        </vxe-toolbar>
                     <el-row>
-                        <vxe-table :max-height="height-300"
-                        size="small"
-                        resizable
-                        border
-                        highlight-hover-row
-                        align="left" style="margin:0 18px;"
-                        :data="tableData">
-                            <vxe-table-column type="seq" width="60"></vxe-table-column>
-                            <vxe-table-column field="name" title="Name"></vxe-table-column>
-                            <vxe-table-column field="sex" title="Sex"></vxe-table-column>
-                            <vxe-table-column field="age" title="Age"></vxe-table-column>
-                            <vxe-table-column field="address" title="Address" show-overflow></vxe-table-column>
-                        </vxe-table>
+                        <el-col :span="18">
+                            <vxe-table 
+                            :height="height-300"
+                            size="small"
+                            resizable
+                            border
+                            stripe
+                            highlight-hover-row
+                            show-overflow
+                            align="left" style="margin:0 18px;"
+                            :footer-method="footerMethod"
+                            min-width="50px"
+                            show-footer
+                            :footer-cell-class-name="footerCellClassName"
+                            :header-cell-style="headerCellStyle"
+                            :cell-style="cellStyle"
+                            :data="tableData">
+                                <vxe-table-column type="seq" width="60"></vxe-table-column>
+                                <template v-if="tableCell.length>0">
+                                    <vxe-table-column  v-for="(cel,index) in tableCell" :key="index" :field="cel.id" :title="cel.labelString" :width="cel.numChar*8" :formatter="cel.type==3?formatFixedNumber:''" :align="cel.type==3?'right':'center'" ></vxe-table-column>
+                                </template>
+                                <!-- <vxe-table-column field="name" title="摘要" ></vxe-table-column>
+                                <vxe-table-column field="sex" title="科目"></vxe-table-column>
+                                <vxe-table-column field="rmbd" title="借方金额" align="right"></vxe-table-column>
+                                <vxe-table-column field="fcyc" title="贷方金额" align="right"></vxe-table-column>
+
+                                <vxe-table-column field="address" title="Address" show-overflow></vxe-table-column> -->
+                                <vxe-table-column title="操作" width="100" show-overflow align="center">
+                                    <template #default="{ row }">
+                                    <vxe-button type="text" icon="el-icon-edit-outline" @click="editEvent(row)" status="primary"></vxe-button>
+                                    <vxe-button type="text" icon="el-icon-delete" status="danger"></vxe-button>
+                                    </template>
+                                </vxe-table-column>
+                            </vxe-table>
+                        </el-col>
+                        <el-col :span="6">
+                             <el-card>
+                                <div slot="header" class="clearfix">
+                                    <span>辅助项</span>
+                                </div>
+                                    <el-row :style="'height:'+(height-350)+'px;'"></el-row>
+                             </el-card>
+                        </el-col>
                     </el-row>
+                   
+                   
                 </el-card>
-                <el-card>
+                <!-- <el-card>
                     <el-row class="bip-row" :gutter="4" v-if="bottomLay" :style="'margin-top:10px;'">
                         <lay-cell :laycell="bottomLay.comp" :env="env" ></lay-cell>
                     </el-row>
-                </el-card>
+                </el-card> -->
              </el-form>
         </div>
     </el-row>
@@ -60,6 +97,8 @@ import CRecord from '../../../classes/pub/CRecord';
 import CData from '../../../classes/pub/CData';
 import BipLayConf from "@/classes/ui/BipLayConf";
 import LayCell from '@/components/layout/LayCell.vue';
+import {CurrUtils} from '@/utils/CurrUtils'
+let currutil = CurrUtils.curr
 let icl = CommICL;
 let tools = BIPUtil.ServApi
 @Component({
@@ -86,6 +125,7 @@ export default class VoucherApp extends Vue{
     height:number = 400;
     billstyle:string = '';
     tableData:Array<any>=[];
+    tableCell:Array<any>=[];
     created(){
         this.height = document.documentElement.clientHeight
         if(this.height>70){
@@ -108,14 +148,14 @@ export default class VoucherApp extends Vue{
         }
         await this.uriParamsChange();
         this.tableData= [
-                { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'vxe-table 从入门到放弃' },
-                { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
-                { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
-                { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 23, address: 'vxe-table 从入门到放弃' },
-                { id: 10005, name: 'Test5', role: 'Develop', sex: 'Women', age: 30, address: 'Shanghai' },
-                { id: 10006, name: 'Test6', role: 'Designer', sex: 'Women', age: 21, address: 'vxe-table 从入门到放弃' },
-                { id: 10007, name: 'Test7', role: 'Test', sex: 'Man', age: 29, address: 'vxe-table 从入门到放弃' },
-                { id: 10008, name: 'Test8', role: 'Develop', sex: 'Man', age: 35, address: 'vxe-table 从入门到放弃' }
+                { adic: 10001, name: 'Test1', role: 'Develop', sex: 'Man', rmbd: 28, remark: 'vxe-table 从入门到放弃vxe-table 从入门到放弃vxe-table 从入门到放弃vxe-table 从入门到放弃' },
+                { adic: 10002, name: 'Test2', role: 'Test', sex: 'Women', rmbd: 22, remark: 'Guangzhou' },
+                { adic: 10003, name: 'Test3', role: 'PM', sex: 'Man', rmbc: -32, remark: 'Shanghai' },
+                { adic: 10004, name: 'Test4', role: 'Designer', sex: 'Women', rmbd: 23, remark: 'vxe-table 从入门到放弃' },
+                { adic: 10005, name: 'Test5', role: 'Develop', sex: 'Women', rmbd: 30, remark: 'Shanghai' },
+                { adic: 10006, name: 'Test6', role: 'Designer', sex: 'Women', rmbd: 21, remark: 'vxe-table 从入门到放弃' },
+                { adic: 10007, name: 'Test7', role: 'Test', sex: 'Man', rmbd: 29, remark: 'vxe-table 从入门到放弃' },
+                { adic: 10008, name: 'Test8', role: 'Develop', sex: 'Man', rmbd: 35, remark: 'vxe-table 从入门到放弃' }
               ]
     }
 
@@ -123,6 +163,30 @@ export default class VoucherApp extends Vue{
         console.log(btn);
     }
 
+    editEvent(row:any){
+        console.log(row)
+    }
+
+    insertPZRow(){}
+
+
+
+    footerMethod ({ columns, data }:any) {
+        return [
+        columns.map((column :any, columnIndex :any) => {
+            if (columnIndex === 0) {
+                return '合计'
+            }
+            if (['rmbd'].includes(column.property)) {
+            return this.sumNum(data, column.property)
+            }
+            if (['rmbc'].includes(column.property)) {
+            return this.sumNum(data, column.property)
+            }
+            return null
+        })
+        ]
+    }
     async uriParamsChange() {
         if (this.uriParams&&this.uriParams.pcell) {
             let pcell = this.uriParams.pcell
@@ -160,10 +224,9 @@ export default class VoucherApp extends Vue{
                 );
                 let buid = this.uriParams.pflow
                 this.headerLay = this.lay.compconfs[0];
-                console.log(this.headerLay);
                 let btm:any = this.lay.compconfs[2];
                 this.bottomLay = btm.comp.compconfs[1];
-                console.log(this.bottomLay);
+                
                 if(buid){
                     let res1 = await tools.getBULinks(buid);
                     let rtn1 = res1.data;
@@ -172,6 +235,11 @@ export default class VoucherApp extends Vue{
                         this.dsm.setOpera(ope)
                     }
                 }
+                this.dsm.createRecord();
+                 let tb1:any = this.lay.compconfs[1];
+                this.tableCell = tb1.comp.uiCels;
+                // console.log(this.tableCell);
+
             } else {
                 this.$notify.error("没有获取到对象定义");
             }
@@ -184,6 +252,67 @@ export default class VoucherApp extends Vue{
         });
     }
 
+    footerCellClassName ({ $rowIndex, columnIndex }:any) {
+        return 'col-hj'
+    }
+
+     // 四舍五入,默认两位数
+    formatFixedNumber ({ cellValue }:any, digits = 2) {
+        return  currutil.currency(cellValue,'',digits);
+    }
+
+    headerCellStyle ({ column, columnIndex }:any) {
+        if(columnIndex>0&&this.tableCell){
+            let rr = this.tableCell[columnIndex-1];
+            if(rr){
+                if (rr.isReq) {
+                    return {
+                        // backgroundColor: '#f60',
+                        color: 'red'
+                    }
+                }
+                if (rr.type === 3) {
+                    return {
+                        color: 'green'
+                    }
+                }
+            }
+        }
+
+    }
+
+    cellStyle ({ row,columnIndex }:any) {
+         if(columnIndex>0&&this.tableCell){
+            let rr = this.tableCell[columnIndex-1];
+            if(rr&&rr.type ===3 ){
+                let v = row[rr.id];
+                if (v<0) {
+                    return {
+                        color: 'red'
+                    }
+                }
+            }
+        }
+    }
+
+    sumNum (list:any, field:any) {
+        let count = 0
+        list.forEach((item:any) => {
+            let nd =  Number(item[field]);
+            if(isNaN(nd))
+                nd = 0.0;
+            count += nd;
+        });
+        return currutil.currency(count,'',2);
+    }
+
 
 }
 </script>
+
+<style>
+.col-hj{
+    background-color: #56b9bc;
+    color: #fff;
+}
+</style>
