e9c17ae0bf3bf4b21478f3ad4efa1fbeca854da1
LiZhengguo <lzg163email@163.com>
2021-06-18 18:42:25 +0800

    按钮根据单据状态显示

--- src/components/menubar/BipMenuBarUi.vue
+++ src/components/menubar/BipMenuBarUi.vue
@@ -1,8 +1,8 @@
 <template>
     <el-row class="menubar">
-        <el-button-group v-if="mbs">
+        <el-button-group v-if="mbs && initOk">
             <template  v-for="(btn,index) in mbs.menuList">
-                <el-button class="bip-menu-bar" :class="btn.type?'bip_btn_'+btn.type:'bip_btn_default'" :key="index" v-if="btn.dlgType == '' || showDlg" :size="btn.size" @click.native="invokecmd(btn)" :disabled="!btn.enable">     
+                <el-button v-show="btnShow[index]" class="bip-menu-bar" :class="btn.type?'bip_btn_'+btn.type:'bip_btn_default'" :key="index" v-if="btn.dlgType == '' || showDlg" :size="btn.size" @click.native="invokecmd(btn)" :disabled="!btn.enable">     
                     <template v-if="btn.hasIcon">
                         <template v-if="btn.icon&&btn.bIconleft">
                             <i :class="btn.icon"></i>{{btn.name}}
@@ -26,28 +26,54 @@
 //10:审批;关联;文件;图形;引用;15:功能文字;统计;外部SQL;展开;批量打印;
 //20:全部打印;邮件;无表格线;行单元底线;行单元边框;25:普通表头;关闭速查;执行可改
 import { Component, Vue, Provide, Prop, Watch } from "vue-property-decorator";
-import { State, Action, Getter, Mutation } from 'vuex-class';
-import { CommICL } from "@/utils/CommICL";
 import BipMenuBar from '@/classes/pub/BipMenuBar';
 import CDataSet from '@/classes/pub/CDataSet';
 @Component({})
 export default class BipMenuBarUI extends Vue{
     @Prop() mbs!:BipMenuBar;
     @Prop() cds!:CDataSet
-    @Provide() bInsert:boolean = true;
-    @Provide() showDlg:boolean = true;
+    bInsert:boolean = true;
+    showDlg:boolean = true;
+    btnShow:any = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,];
+    initOk:boolean = false;
     invokecmd(btn:any){
-        console.log(btn)
         this.$emit('invokecmd',btn);
     }
     mounted(){ 
-        
+        this.getBtnShow();
     }
     beforeDestroy(){
         
     }
     ReportTableShape(){
         this.showDlg = !this.showDlg;
+    }
+
+    @Watch("cds.currRecord.c_state")
+    getBtnShow(){
+        console.log("getBtnShow")
+        for(var i=0;i< this.mbs.menuList.length;i++){
+            let btn = this.mbs.menuList[i];
+            if(btn.cmd=='COPY' || btn.cmd =='DEL'){
+                if(this.cds.currRecord){
+                    if ((this.cds.currRecord.c_state & 1) > 0) {
+                        this.btnShow[i] = false;
+                    }else{
+                        this.btnShow[i] = true;
+                    }
+                }else{
+                    this.btnShow[i] = false;
+                }
+            }else{
+                this.btnShow[i] = true;
+            }
+        }
+        this.initOk = true;
+        this.$forceUpdate();
+    }
+    @Watch("mbs")
+    menuListChange(){
+        this.getBtnShow();
     }
 }
 </script>
