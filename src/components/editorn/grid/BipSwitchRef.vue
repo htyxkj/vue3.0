7b8d8df8fcabf621f593ec12ec052287449c5aa4
LiZhengguo <lzg163email@163.com>
2021-09-02 09:52:11 +0800

    报表 switch 显示

---
+++ src/components/editorn/grid/BipSwitchRef.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="bip-show">
+       <el-switch v-model="model1" disabled></el-switch>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue,  Prop, Watch } from "vue-property-decorator"
+@Component({})
+export default class BipSwitchRef extends Vue{
+    @Prop() model!:string
+    model1:boolean = false;
+    mounted(){
+       this.initModel();
+    }
+    initModel(){
+        if(this.model && this.model == '1'){
+            this.model1 = true;
+        }else{
+            this.model1 = false;
+        }
+    }
+    @Watch("model")
+    modelChange(){
+        this.initModel();
+    }
+}
+</script>
+
+
+<style lang="scss">
+.bip-show{
+    width: 100%;
+    padding: 0 0;
+}
+</style>
