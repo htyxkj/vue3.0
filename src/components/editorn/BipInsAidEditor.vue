12a6ad09e9d30cb5c91dd0ed1c33874df7b9b460
LiZhengguo <lzg163email@163.com>
2021-10-10 19:27:40 +0800

    辅助修改

--- src/components/editorn/BipInsAidEditor.vue
+++ src/components/editorn/BipInsAidEditor.vue
@@ -244,7 +244,8 @@ export default class BipInsAidEditor extends Vue{
                 let oneVal = val[i];
                 let key = oneVal[this.bipInsAid.cells.cels[0].id] 
                 this.model1 += key+";" 
-                this.setAidValue({key:this.bipInsAid.id+"_"+key,value:oneVal});
+                let key1 = ICL.AID_KEY+this.aidMarkKey+this.linkName+"_"+key
+                this.setAidValue({key:key1,value:oneVal});
                 strval+=oneVal[this.bipInsAid.cells.cels[1].id]+";"
             }
             if (this.mulcols) {
@@ -356,7 +357,12 @@ export default class BipInsAidEditor extends Vue{
                     if(!vrs){
                         let str = window.sessionStorage.getItem(key)
                         if(!str){
-                            let vvs = {id:this.linkName,key:key,cont:cont}
+                            let groupV = null;
+                            if(this.bipInsAid.bType =="CGroupEditor"){
+                                let groupFld = this.bipInsAid.groupFld;
+                                groupV = this.cds.currRecord.data[groupFld];
+                            }
+                            let vvs = {id:this.linkName,key:key,cont:cont,groupV:groupV}
                             let res = await this.fetchInsDataByCont(vvs)
                             if(res.data.id ==0){
                                 vrs = res.data.data.data.values[0]
